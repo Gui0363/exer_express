04a6e55460fa257614c6e4d3ceb014b5797ca0dd
GuiSouza <guilhermespublic@gmail.com>
2019-05-03 09:26:05 -0300

    Adicionando as requisicoes com corpo, e Body Parser funcionando diretamente com json e urlencoded

--- index.js
+++ index.js
@@ -1,8 +1,77 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-app.get('/opa', (req, res) => {
-  res.send('Estou Bem')
+const saudacao = require('./saudacaoMid')
+
+app.use(bodyParser.text())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(saudacao('Gui'))
+
+app.use((req, res, next) => {
+  console.log('Sera que serei chamado?')
+  next()
+})
+
+app.get('/clientes/relatorio', (req, res) => {
+  res.send(
+    `Cliente relatório: completo = ${req.query.completo} ano = ${req.query.ano}`
+  )
+})
+
+app.post('/corpo', (req, res) => {
+  /*
+    let corpo = ''
+  req.on('data', function(parte) {
+    corpo += parte
+  })
+
+  req.on('end', function() {
+    res.send(corpo)
+  })
+  */
+  res.send(req.body)
+})
+
+app.get('/clientes/:id', (req, res) => {
+  res.send(`Cliente ${req.params.id} selecionado`)
+})
+
+app.get('/opa', (req, res, next) => {
+  console.log('Durante...')
+  res.json([
+    {
+      id: 7,
+      name: 'Pablo',
+      position: 1,
+    },
+    {
+      id: 70,
+      name: 'Paola',
+      position: 2,
+    },
+    {
+      id: 47,
+      name: 'Patolino',
+      position: 3,
+    },
+  ])
+
+  /*
+    res.json({
+    name: 'iPad 32Gb',
+    price: 1899.0,
+    discount: 0.12,
+  })
+
+  */
+  next()
+})
+
+app.use((req, res) => {
+  console.log('Depois...')
 })
 
 app.listen(3000, () => {
